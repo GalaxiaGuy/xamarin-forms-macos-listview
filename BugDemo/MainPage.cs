413951a6a905878900c457ca2b047083b2881e68
Oliver Brown <oliver.brown@xdesign.com>
2019-04-26 11:08:07 +0100

    Initial commit.

---
+++ BugDemo/MainPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BugDemo
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BackgroundColor = Color.Red;
+            Content = new ListView
+            {
+                Margin = new Thickness(20),
+                BackgroundColor = Color.FromRgba(0, 0, 1, 0.5), 
+                ItemsSource = new string[] { "Row 1", "Row 2", "Row 3" }
+            };
+        }
+    }
+}
